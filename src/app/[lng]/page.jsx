fa360bac193270a2a8667710013d6ac38af7ec73
Mikhail Mogilnikov <mikhail.mogilnikov02@gmail.com>
2024-02-24 16:31:43 +0300

    feat: add Menu and Map entries

--- src/app/[lng]/page.jsx
+++ src/app/[lng]/page.jsx
@@ -1,3 +1,11 @@
+import { MapEntry } from '@/app-map/entries/MapEntry';
+import { MenuEntry } from '@/app-menu/entries/MenuEntry';
+
 export default function Main() {
-  return <main className='w-full min-h-dvh overflow-hidden' />;
+  return (
+    <main className='w-full min-h-dvh overflow-hidden'>
+      <MenuEntry />
+      <MapEntry />
+    </main>
+  );
 }
