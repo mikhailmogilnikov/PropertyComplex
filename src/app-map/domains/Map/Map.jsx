78ae578a41875d9232415cedcc30ba6d35a80b7d
Mikhail Mogilnikov <mikhail.mogilnikov02@gmail.com>
2024-02-27 14:16:59 +0300

    fix: MapEntry import

---
+++ src/app-map/domains/Map/Map.jsx
@@ -0,0 +1,28 @@
+/* eslint-disable func-names */
+/* eslint-disable react/no-this-in-sfc */
+
+import { useMemo, useRef } from 'react';
+import { useTheme } from 'next-themes';
+import { observer } from 'mobx-react-lite';
+import { useStore } from '@/platform/providers/StoreProvider/store';
+
+import SvgComponent from '@/app-map/svgs/SvgComponent';
+import useD3 from '@/app-map/hooks/useD3';
+
+export const Map = observer(() => {
+  const svgRef = useRef(null);
+  const { theme, resolvedTheme } = useTheme();
+  const { databaseStore } = useStore();
+
+  const { isInitMap } = useD3({ svgRef, theme, resolvedTheme, databaseStore });
+  const visibility = useMemo(
+    () => (isInitMap ? 'visible' : 'invisible'),
+    [isInitMap],
+  );
+
+  return (
+    <div className='w-full h-[100dvh] cursor-grab'>
+      <SvgComponent svgRef={svgRef} className={`${visibility}`} />
+    </div>
+  );
+});
--- src/app-map/entries/MapEntry.jsx
+++ src/app-map/entries/MapEntry.jsx
@@ -1,28 +1,12 @@
-/* eslint-disable func-names */
-/* eslint-disable react/no-this-in-sfc */
+'use client';
 
-"use client";
+import dynamic from 'next/dynamic';
 
-import { useMemo, useRef } from "react";
-import { useTheme } from "next-themes";
-import { observer } from "mobx-react-lite";
-import SvgComponent from "../svgs/SvgComponent";
-import { useStore } from "@/platform/providers/StoreProvider/store";
-import useD3 from "../hooks/useD3";
+const DynamicMap = dynamic(
+  () => import('../domains/Map').then((mod) => mod.Map),
+  {
+    ssr: false,
+  },
+);
 
-
-
-export const MapEntry = observer(() => {
-  const svgRef = useRef(null);
-  const { theme, resolvedTheme } = useTheme();
-  const { databaseStore } = useStore();
-
-  const {isInitMap} =  useD3({ svgRef, theme, resolvedTheme, databaseStore });
-  const visibility = useMemo(() => isInitMap ? 'visible' : 'invisible', [isInitMap]);
-
-  return (
-    <div className="w-full h-[100dvh] cursor-grab bg-white">
-       <SvgComponent svgRef={svgRef} className={`${visibility}`}/>
-    </div>
-  );
-});
+export const MapEntry = () => <DynamicMap />;
